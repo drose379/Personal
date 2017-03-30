5d0dcf39a10eb08d02f4be318df646f442658a13
Dylan <dylanrose60@gmail.com>
2017-03-30 19:50:14 -0400

    fix

--- api/add-rating.php
+++ api/add-rating.php
@@ -10,7 +10,11 @@ $current = $info["rates"];
 // Add the newRate to the end of the array
 $current[] = $newRate;
 
-file_put_contents( "rates.json", json_encode( $current ) );
+$response = [];
+$response["ratecount"] = 2;
+$response["averagerate"] = 3;
+$response["rates"] = $current;
+file_put_contents( "rates.json", json_encode( $response ) );
 
 
 // Loop through the array with the newRate, add them all up
@@ -23,6 +27,7 @@ foreach( $current as $rate ) {
 // Divide that by the number of items in the array to get the average (round decimal down)
 
 header('Access-Control-Allow-Origin: *');
+
 error_log( json_encode( file_get_contents( "rates.json" ) ) );
 
 
