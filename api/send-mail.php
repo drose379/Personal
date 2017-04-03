749bfa576c500705c309dc4afbd76975ed7a89e2
Dylan <dylanrose60@gmail.com>
2017-04-03 14:31:37 -0400

    testing

--- api/send-mail.php
+++ api/send-mail.php
@@ -8,10 +8,12 @@ $data = json_decode( file_get_contents( "php://input" ), true );
 $mailgun = new Mailgun( 'key-9f6d81b46519474e5b6fbecebf47b517' );
 $domain = "sandboxde33fec0555248398f436cf70f466920.mailgun.org";
 
+error_log( json_encode( $data ) );
+
 $sendResult = $mailgun->sendMessage(
   $domain,
   array(
-    'from' => $data['name'],
+    'from' => $data['name'] . " " . $data['replyto'],
     'to' => "Dylan <dylanrose60@gmail.com>",
     'subject' => "From Website",
     "text" => $data['message']
