9526d8b1286c2850602b2c7e6b8e4678b992c23d
Dylan <dylanrose60@gmail.com>
2017-04-03 14:24:07 -0400

    requiring correct file

--- api/send-mail.php
+++ api/send-mail.php
@@ -4,3 +4,16 @@ require '../vendor/autoload.php';
 use Mailgun\Mailgun;
 
 $data = json_decode( file_get_contents( "php://input" ), true );
+
+$mailgun = Mailgun::create( 'key-9f6d81b46519474e5b6fbecebf47b517' );
+$domain = "sandboxde33fec0555248398f436cf70f466920.mailgun.org";
+
+$sendResult = $mailgun->sendMessage(
+  $domain,
+  array(
+    'from' => "Test Person <dylanrose60@gmail.com>",
+    'to' => "Dylan <dylanrose60@gmail.com>",
+    'subject' => "Dude it work?",
+    "text" => "Yes, asshole."
+  )
+);
